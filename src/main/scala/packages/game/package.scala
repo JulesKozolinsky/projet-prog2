0d591eba98342e4201d9c7621e6ae6fbea34236e
Alice Rixte <alice.rixte@gmail.com>
2016-02-01 16:35:10 +0100

    Spécification de gui.

--- src/main/scala/packages/game/package.scala
+++ src/main/scala/packages/game/package.scala
@@ -2,7 +2,7 @@ package packages
 
 /** Contient des classes permettant la gestion du déroulement du jeu.
   * 
-  * Description du contenu du package
+  * Contient les classe Level et Round. Un niveau est une suite de rounds. Entre chaque round (ie une vague de monstres), le joueur peu plasser de nouvelles tours pour défendre le passage.
   */
 
 package object game {
--- src/main/scala/packages/gui/gui.scala
+++ src/main/scala/packages/gui/gui.scala
@@ -3,6 +3,7 @@ package packages
 package gui
 
 import swing._
+import javax.swing.Icon
 
 /** Main Frame de la GUI
   */
@@ -11,3 +12,30 @@ class MainFrameGUI extends swing.MainFrame {
   title = "Tower defense"
 }
 
+/** Contient la grille des cases du jeu
+  * 
+  * @param nb_lines Nombre de lignes dans la grille
+  * @param nb_columns Nombre de colonnes dans la grille
+  * TODO : remplacer les paramètres par les paramètres de l'objet map.
+  */
+
+class Grid(nb_line:Int, nb_columns:Int) extends GridPanel(nb_line, nb_columns) /*TODO : taille map*/ {
+
+}
+
+/** Permet de choisir entre plusieurs tours
+  * 
+  * @param : Tableau contenant le nom des fichiers correspondant aux icônes des différentes tours.
+*/
+class TowerChoice(files:Array[String]) extends GridPanel(1,files.length) {
+
+}
+
+/** Permet d'afficher une information sur le jeu avec une icône et un texte 
+  * 
+  * Exemples : nombre de vie, argent restant
+  * @param file Fichier contenant une icône
+  */
+class InfoGame(file : String) extends GridPanel(1,2){
+
+}
