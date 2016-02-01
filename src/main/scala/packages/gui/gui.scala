a567996bb78e3dfb137f43f093964398c98bc9d2
Alice Rixte <alice.rixte@gmail.com>
2016-02-01 21:12:17 +0100

    Ajout de quelques éléments de l'interface.

--- src/main/scala/packages/gui/gui.scala
+++ src/main/scala/packages/gui/gui.scala
@@ -4,6 +4,11 @@ package gui
 
 import swing._
 import javax.swing.Icon
+import javax.swing.border.Border
+import javax.swing.BorderFactory
+import java.awt.Color
+import sugar._
+
 
 /** Main Frame de la GUI
   */
@@ -29,13 +34,14 @@ class GamePanel extends BorderPanel {
   * Exemple : information sur la vie et l'argent, options sur la tour à utiliser
   */
 class GameOptions extends BorderPanel {
+  border = BorderFactory.createMatteBorder(5, 5, 5, 5, new Color(0,0,0,0))
   /** Affichage du nombre de vies */
   val life_info = new InfoGame("")
   /** Affichage argent restant */
   val money_info = new InfoGame("")
   /** Choix de la tour */
   val tower_choice = new TowerChoice(Array("Tour1","Tour2"))
-  add(new GridPanel(2,1){contents += life_info;contents += money_info}, BorderPanel.Position.West) //on affiche la vie et l'argent dans une colonne tout à gauche
+  add(new GridPanel(2,1){vGap = 10; contents += life_info;contents += money_info}, BorderPanel.Position.West) //on affiche la vie et l'argent dans une colonne tout à gauche
   add(tower_choice , BorderPanel.Position.East) //on affiche le choix des tours tout à droite
 }
 
@@ -44,9 +50,18 @@ class GameOptions extends BorderPanel {
   * @param nb_lines Nombre de lignes dans la grille
   * @param nb_columns Nombre de colonnes dans la grille
   */
-
-class GameGrid(nb_line:Int, nb_columns:Int) extends GridPanel(nb_line, nb_columns) {
-
+class GameGrid(nb_line:Int, nb_columns:Int) extends PosGridPanel(nb_line, nb_columns) {
+  for(i<-0 to this.rows - 1) //rows et columns sont héritées de GridPanel
+    {
+      for(j<-0 to this.columns - 1)
+        {
+          contents += new Button("Blub")
+        }
+    }
+  (this(new Position(1,2)) match {
+    case but : Button => but
+    case _  => throw new ClassCastException
+  }).text = "Changé"
 }
 
 /** Permet de choisir entre plusieurs tours
@@ -54,6 +69,7 @@ class GameGrid(nb_line:Int, nb_columns:Int) extends GridPanel(nb_line, nb_column
   * @param : Tableau contenant le nom des fichiers correspondant aux icônes des différentes tours.
   */
 class TowerChoice(files:Array[String]) extends GridPanel(1,files.length) {
+  hGap = 7
   for(i<-0 to files.length - 1)
     {
       contents += new Button(files(i))
