65a1aef3173ab63bf26fd5f7afbda0d1c1d7cd77
JulesKozolinsky <jules.kozolinsky@ens-cachan.fr>
2016-02-05 17:48:47 +0100

    [map] commentaires + cartes tower

--- src/main/scala/packages/map/map.scala
+++ src/main/scala/packages/map/map.scala
@@ -13,39 +13,40 @@ object Map
   /** largeur de la carte */
   val width : Int = 21
 
-  /** la carte ground représente les tours, les obstacles, i.e. ce qui ne bouge a priori pas */
+  /** la carte ground représente les obstacles, i.e. ce qui ne bouge a priori pas */
   var ground = Array.ofDim[List[Tileable]](height,width)
+
+  /** la carte towers représente les tours (une tour ou liste vide)*/
+  var towers = Array.ofDim[List[TowerType]](height,width)
 
   /** la carte monsters représente les montres, il peut y en avoir plusieurs sur une même case,
     * d'où l'ensemble (Set) de monstres par case
     */
-  var monsters = Array.ofDim[Set[Moveable]](height,width)
+  var monsters = Array.ofDim[Set[Monster]](height,width)
 
-  /** le chemin où les monstrers peuvent se déplacer*/
+  /** le chemin où les monstrers peuvent se déplacer */
   var path : Array[Position] = {new Array[Position] (0)}
 
   /** là où sont stockées les images associées aux tours*/
   var tower_resources : Array[String] = {Array("tower1.png","tower2.png")}
 
-  /** Calcule le chemin à partir d'une position jusqu'à la fin (appelé au début du round)*/
+  /** Calcule le chemin à partir d'une position jusqu'à la fin (appelé au début du round) */
   def compute_path () : Array[Position] = {new Array [Position] (0)}
 
-  /** Supprime le monstre monster de la 1ere position et l'ajoute sur la deuxieme*/
-  def move_monster (monster:Monster) (p1:Position) (p2:Position) = {}
+  /** Supprime le monstre monster de la 1ere position et l'ajoute sur la deuxieme */
+  def move_monster (monster:Monster) (p1:Position) (p2:Position) : Unit = {}
 
-  /** */
+  /** supprime le monstre */
   def remove_monster (monster:Monster) = {}
 
   /** Donne la case suivante d'un monster à partir du chemin*/
   def next_case (p:Position) : Position = {new Position (0,0)}
 
-  /** */
+  /** renvoie un tableau de monsters susceptibles d'être touchés par une tour donnée*/
   def get_targets (tower:Tower) : Array[Monster] = {new Array[Monster] (0)}
 
-  /** */
+  /** Renvoie vrai et crée une nouvelle tour si possible, sinon renvoie faux*/
   def new_tower (t:TowerType) (p:Position) : Boolean = {true}
 
-  /** */
-  def free_for_tower (p:Position) : Boolean = {true /* la poisition n'est pas dans le chemin */}
 }
 // get_targets (Tour (range,position)) Vector(2 pts) (norme)
