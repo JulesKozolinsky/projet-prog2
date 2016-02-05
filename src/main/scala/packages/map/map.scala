89dc01ec39b8c40e06a5bb8a86ec3313bded6439
JulesKozolinsky <jules.kozolinsky@ens-cachan.fr>
2016-02-05 18:13:32 +0100

    [map] arguments avec des virgules...

--- src/main/scala/packages/map/map.scala
+++ src/main/scala/packages/map/map.scala
@@ -25,19 +25,33 @@ object Map
   var monsters = Array.ofDim[Set[Monster]](height,width)
 
   /** le chemin où les monstrers peuvent se déplacer */
-  var path : Array[Position] = {new Array[Position] (0)}
+  var path : Array[Position] = {compute_path()}
 
   /** là où sont stockées les images associées aux tours*/
   var tower_resources : Array[String] = {Array("tower1.png","tower2.png")}
 
   /** Calcule le chemin à partir d'une position jusqu'à la fin (appelé au début du round) */
-  def compute_path () : Array[Position] = {new Array [Position] (0)}
+  def compute_path () : Array[Position] = {
+      var path = new Array[Position](width)
+      var c = 0;
+      for( c <- 0 to (width-1)) {
+        path(c) = new Position (height/2,c)
+      }
+      path
+     }
 
   /** Supprime le monstre monster de la 1ere position et l'ajoute sur la deuxieme */
-  def move_monster (monster:Monster) (p1:Position) (p2:Position) : Unit = {}
+  def move_monster (monster:Monster,p1:Position,p2:Position) : Unit = {
 
-  /** supprime le monstre */
-  def remove_monster (monster:Monster) = {}
+  }
+
+  /** supprime le monstre à la position p de la carte monsters*/
+  def remove_monster (monster:Monster,p:Position)= {}
+
+  /** ajoute un monstre à la position p de la carte monsters*/
+  def add_monster (monster:Monster,p:Position) = {
+
+  }
 
   /** Donne la case suivante d'un monster à partir du chemin*/
   def next_case (p:Position) : Position = {new Position (0,0)}
@@ -46,7 +60,7 @@ object Map
   def get_targets (tower:Tower) : Array[Monster] = {new Array[Monster] (0)}
 
   /** Renvoie vrai et crée une nouvelle tour si possible, sinon renvoie faux*/
-  def new_tower (t:TowerType) (p:Position) : Boolean = {true}
+  def new_tower (t:TowerType,p:Position) : Boolean = {true}
 
 }
 // get_targets (Tour (range,position)) Vector(2 pts) (norme)
