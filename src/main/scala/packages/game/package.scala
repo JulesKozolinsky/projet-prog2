51534d0f2176866ee29f3fc74e11e597fdc68eb3
JulesKozolinsky <jules.kozolinsky@ens-cachan.fr>
2016-02-22 19:11:52 +0100

    [game] money = 100000 pour les tests

--- src/main/scala/packages/game/package.scala
+++ src/main/scala/packages/game/package.scala
@@ -1,7 +1,7 @@
 package packages
 
 /** Contient des classes permettant la gestion du déroulement du jeu.
-  * 
+  *
   * Contient les classe Level et Round. Un niveau est une suite de rounds. Entre chaque round (ie une vague de monstres), le joueur peu plasser de nouvelles tours pour défendre le passage.
   */
 
@@ -10,7 +10,7 @@ package object game {
   val tick = 100
 
   /** Argent du joueur */
-  var money = 100
+  var money = 100000
 
   /** Nombre de vies restantes */
   var life = 10
