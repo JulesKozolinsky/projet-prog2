99d16c26f138968231276c23b4aeb54a0b64e31d
sovietdu33 <gabriel.boudy@laposte.net>
2016-03-11 21:09:18 +0100

    Pouf pouf

--- src/main/scala/packages/entities/package.scala
+++ src/main/scala/packages/entities/package.scala
@@ -1,4 +1,5 @@
 package packages
+import sugar._
 
 /** Contient les entités du jeu, i.e. les différents types d'éléments qui vont apparaître sur la map */
 
@@ -7,7 +8,7 @@ package packages
 package object entities
 {
 
-  /** Cette fonction prend le premier */
+  /** Cette fonction renvoie le premier monstre qui apparaît dans la liste des cibles (on va dire aléatoire car compliqué) */
   def lazi (l:List[Monster]) : List[Monster] =
   {
     if (l.isEmpty)
@@ -20,21 +21,28 @@ package object entities
     }
   }
 
+  /** Cette fonction renvoie les monstres se trouvant sur la case la plus peuplée parmis les cases visibles */
+  def case_max (l:List[Monster]) : List[Monster] =
+  {
+    var pos_set = Set[(Position,Int)] ()
+    l.foreach { (m:Monster) => pos_set = pos_set + new Tuple2(m.pos,0) }
+    l.foreach { (m:Monster) => var Some(m_nbr) = pos_set.find {(x:Tuple2[Position,Int]) => (m.pos == x._1)}
+      pos_set = (pos_set - m_nbr) + (new Tuple2(m_nbr._1, m_nbr._2 + 1))   } // a ce stade on a un set avec les positions et es monstres 
+    l
+  }
 
-def case_max (l:List[Monster]) : List[Monster] = 
+  /** Cette fonction renvoie les monstres se trouvant sur la case la plus avancée dans le parcours des monstres vers La Commune */
+  def case_closest (l:List[Monster]) : List[Monster] =
   {
     l
   }
 
 
-def case_closest (l:List[Monster]) : List[Monster] = 
+  /** Cette fonction renvoie un monstre se trouvant sur la case la plus avancée dans le parcours des monstres vers La Commune */
+  def closest (l:List[Monster]) : List[Monster] =
   {
     l
   }
-
-
-
-
 
 
 }
