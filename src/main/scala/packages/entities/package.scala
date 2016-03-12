10935ef453ca0b976cd8bc78bc13e297add57c6e
Boudy <boudy@skynet>
2016-03-13 00:42:02 +0100

    Modifs mineures pour les priorités

--- src/main/scala/packages/entities/package.scala
+++ src/main/scala/packages/entities/package.scala
@@ -9,39 +9,38 @@ package object entities
 {
 
   /** Cette fonction renvoie le premier monstre qui apparaît dans la liste des cibles (on va dire aléatoire car compliqué) */
-  def lazi (l:List[Monster]) : List[Monster] =
+  def lazi (l:List[(Position,List[Monster])]) : List[Monster] =
   {
-    if (l.isEmpty)
-    {
-      l
-    }
-    else
-    {
-      List(l(0))
-    }
+    if (l.isEmpty) {List[Monster]()}
+    else {l(0)._2}
   }
 
   /** Cette fonction renvoie les monstres se trouvant sur la case la plus peuplée parmis les cases visibles */
-  def case_max (l:List[Monster]) : List[Monster] =
+  def case_max (l:List[(Position,List[Monster])]) : List[Monster] =
   {
-    var pos_set = Set[(Position,Int)] ()
-    l.foreach { (m:Monster) => pos_set = pos_set + new Tuple2(m.pos,0) }
-    l.foreach { (m:Monster) => var Some(m_nbr) = pos_set.find {(x:Tuple2[Position,Int]) => (m.pos == x._1)}
-      pos_set = (pos_set - m_nbr) + (new Tuple2(m_nbr._1, m_nbr._2 + 1))   } // a ce stade on a un set avec les positions et es monstres 
-    l
+    //var pos_set = Set[(Position,Int)] ()
+    //l.foreach { (m:Monster) => pos_set = pos_set + new Tuple2(m.pos,0) }
+    //l.foreach { (m:Monster) => var Some(m_nbr) = pos_set.find {(x:Tuple2[Position,Int]) => (m.pos == x._1)}
+      //pos_set = (pos_set - m_nbr) + (new Tuple2(m_nbr._1, m_nbr._2 + 1))   } // a ce stade on a un set avec les positions et es monstres 
+    l(0)._2
   }
 
   /** Cette fonction renvoie les monstres se trouvant sur la case la plus avancée dans le parcours des monstres vers La Commune */
-  def case_closest (l:List[Monster]) : List[Monster] =
+  def case_closest (l:List[(Position,List[Monster])]) : List[Monster] =
   {
-    l
+    l(0)._2
   }
 
-
   /** Cette fonction renvoie un monstre se trouvant sur la case la plus avancée dans le parcours des monstres vers La Commune */
-  def closest (l:List[Monster]) : List[Monster] =
+  def closest (l:List[(Position,List[Monster])]) : List[Monster] =
   {
-    l
+    l(0)._2
+  }
+
+  /** Cette fonction renvoie les monstres se trouvant dans la lignée du monstre le plus avancé */
+  def line (l:List[(Position,List[Monster])]) : List[Monster] =
+  {
+    l(0)._2
   }
 
 
--- src/main/scala/packages/sugar/sugar.scala
+++ src/main/scala/packages/sugar/sugar.scala
@@ -1,6 +1,7 @@
 package packages
 
 package sugar
+import entities._
 
 /** Cette classe doit être utilisée à chaque fois que l'on fait référence à une entité sur la grille
   *
@@ -8,7 +9,7 @@ package sugar
   * @param c Numéro de la colonne
   */
 
-class Position(l_arg:Int, c_arg:Int)
+class Position (l_arg:Int, c_arg:Int)
 {
   var l = l_arg
   var c = c_arg
@@ -22,5 +23,41 @@ class Position(l_arg:Int, c_arg:Int)
 
 
 class Vector (point1:Position,point2:Position)
-{  def norme : Int = math.sqrt((point1.l - point2.l)*(point1.l - point2.l) + (point1.c - point2.c)*(point1.c - point2.c)).toInt
+{
+  def norme : Int = math.sqrt((point1.l - point2.l)*(point1.l - point2.l) + (point1.c - point2.c)*(point1.c - point2.c)).toInt
+}
+
+
+class Targets ()
+{
+  var itself = List[(Position,List[Monster])] ()
+
+  def add_position (p:Position) : Unit = 
+  {
+    var already_exists = false
+    itself.foreach {(x:(Position,List[Monster])) => already_exists = already_exists || (p.equals (x._1) )}
+    if (! already_exists) {itself = new Tuple2(p,List[Monster]())::itself}
+  }
+
+  def add_monster (p:Position,m:Monster) : Unit = 
+  {
+    var Some(pos) = itself.find {(x:(Position,List[Monster])) => (x._1 == p)}
+    itself = itself.filter {(x:(Position,List[Monster])) => !(x==pos)}
+    itself = new Tuple2(pos._1,m::pos._2)::itself
+  }
+
+  def empty_target () : Boolean = 
+  {
+    var found_targets = false
+    itself.foreach {(x:(Position,List[Monster])) => found_targets = found_targets || (! x._2.isEmpty) }
+    !found_targets
+  }
+
+  def simple () : Unit = 
+  {
+    var new_itself = List[(Position,List[Monster])] ()
+    itself.foreach {(x:(Position,List[Monster])) => if (!x._2.isEmpty) {new_itself = x::new_itself}}
+    itself = new_itself    
+  }
+
 }
