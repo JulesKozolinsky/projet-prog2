b95a889f247d0518a4305b879964e945b678efe9
Boudy <boudy@skynet>
2016-03-13 04:00:43 +0100

    début des priorités

--- src/main/scala/packages/entities/package.scala
+++ src/main/scala/packages/entities/package.scala
@@ -18,16 +18,16 @@ package object entities
   /** Cette fonction renvoie les monstres se trouvant sur la case la plus peuplée parmis les cases visibles */
   def case_max (l:List[(Position,List[Monster])]) : List[Monster] =
   {
-    //var pos_set = Set[(Position,Int)] ()
-    //l.foreach { (m:Monster) => pos_set = pos_set + new Tuple2(m.pos,0) }
-    //l.foreach { (m:Monster) => var Some(m_nbr) = pos_set.find {(x:Tuple2[Position,Int]) => (m.pos == x._1)}
-      //pos_set = (pos_set - m_nbr) + (new Tuple2(m_nbr._1, m_nbr._2 + 1))   } // a ce stade on a un set avec les positions et es monstres 
-    l(0)._2
+    var targets = List[Monster]()
+    l.foreach {(x:(Position,List[Monster])) => if (x._2.size > targets.size) {targets = x._2}}
+    targets
   }
 
   /** Cette fonction renvoie les monstres se trouvant sur la case la plus avancée dans le parcours des monstres vers La Commune */
   def case_closest (l:List[(Position,List[Monster])]) : List[Monster] =
   {
+    var targets = List[Monster] ()
+    //path.foreach {(p:Position) => } PROBLEME : JE VEUX ITERER LE PATH DANS LE BON SENS ET JE SAIS PAS CE QUE FAIT FOREACH
     l(0)._2
   }
 
